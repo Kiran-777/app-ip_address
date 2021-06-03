f03bf8e5f4e5bccf7d43505b2d2fc1ede4ed3c92
Kiran-777 <Reddy.Kirankumari@ibm.com>
2021-06-03 09:32:00 +0000

    Lab 2 part 1 of 2

--- main.js
+++ main.js
@@ -50,36 +50,6 @@ function getFirstIpAddress(cidrStr, callback) {
   This section is used to test function and log any errors.
   We will make several positive and negative tests.
 */
-function main() {
-  // Create an array of tests with both valid CIDR and invalid IP subnets.
-  let sampleCidrs = ['172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.10.128/25', '192.168.1.216/30'];
-  let sampleCidrsLen = sampleCidrs.length;
-
-  // Iterate over sampleCidrs and pass the element's value to getFirstIpAddress().
-  for (var i = 0; i < sampleCidrsLen; i++) {
-    console.log(`\n--- Test Number ${i + 1} getFirstIpAddress(${sampleCidrs[i]}) ---`);
-    // Call getFirstIpAddress and pass the test subnet and an anonymous callback function.
-    // The callback is using the fat arrow operator: () => { }
-    getFirstIpAddress(sampleCidrs[i], (data, error) => {
-      // Now we are inside the callback function.
-      // Display the results on the console.
-      if (error) {
-        console.error(`Error returned from GET request: ${error}`);
-      } else {
-        console.log(`Response returned from GET request: ${data}`);
-      }
-    });
-  }
-}
-
-/*
-  Call main to run it.
-*/
-/**
- * Calculates an IPv4-mapped IPv6 address.
- * @param {string} ipv4 - An IPv4 address in dotted-quad format.
- * @return {*} (ipv6Address) - An IPv6 address string or null if a run-time problem was detected.
- */
 function getIpv4MappedIpv6Address(ipv4) {
 
   // Initialize return argument
@@ -123,11 +93,8 @@ function getIpv4MappedIpv6Address(ipv4) {
   }
   return ipv6Address;
 }
-/*
-  This section is used to test function and log any errors.
-  We will make several positive and negative tests.
-*/
-function main() {
+  // Create an array of tests with both valid CIDR and invalid IP subnets.
+  function main() {
   // Create some test data for getFirstIpAddress(), both valid and invalid.
   let sampleCidrs = ['172.16.10.0/24', '172.16.10.0 255.255.255.0', '172.16.10.128/25', '192.168.1.216/30'];
   let sampleCidrsLen = sampleCidrs.length;
@@ -161,3 +128,5 @@ function main() {
     }
   }
 }
+
+main();
